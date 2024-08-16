46455bdb6d9a36f69dccdb9d42efc16ebd8413f2
Folkenciyo <86403857+Folkenciyo@users.noreply.github.com>
2024-08-16 13:09:50 +0200

    added libft, GNL, PRTF  & defines.h

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,6 +1,10 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 # include "MLX42.h"
+# include "defines.h"
+# include "libft.h"
+# include "ft_printf.h"
+# include "get_next_line.h"
 # include "player.h"
 # include <stdlib.h>
 # include <stdio.h>
